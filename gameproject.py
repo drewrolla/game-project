be83778555220d74e891337ed174982c35c955b4
drewrolla <andrewong007@gmail.com>
2022-07-16 21:15:05 -0400

    first commit

---
+++ gameproject.py
@@ -0,0 +1,9 @@
+# this is the first project I am going to do independent of Coding Temple.
+# the idea:
+# create a very basic game - maybe simulate Pokemon moves, not quite sure
+
+class Game():
+
+    def __init__(self):
+        pass
+
