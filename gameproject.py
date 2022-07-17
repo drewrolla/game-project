14c70065edaf8508dc197d60df0b9bc999d6bb9f
drewrolla <andrewong007@gmail.com>
2022-07-16 21:16:38 -0400

    added some comments

--- gameproject.py
+++ gameproject.py
@@ -1,6 +1,7 @@
 # this is the first project I am going to do independent of Coding Temple.
 # the idea:
 # create a very basic game - maybe simulate Pokemon moves, not quite sure
+# also practicing OOP
 
 class Game():
 
